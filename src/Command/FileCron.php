89b624174286fd12710798f1589c1050d0e16644
Philip Maaß <PhilipMaasz@aol.com>
2019-10-18 18:14:00 +0200

    adds file for FileCron

---
+++ src/Command/FileCron.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Philip Maaß
+ * Date: 18.10.19
+ * Time: 18:12
+ * License
+ */
+
+/** TODO implement service to merge files at the end of the day/week */
+
+
