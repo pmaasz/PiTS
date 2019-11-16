2f4c8766bc2e13462e80d0aa8b30b4e25fceb261
Philip Maaß <PhilipMaasz@aol.com>
2019-11-16 22:58:37 +0100

    creates TemperatureSensorService

--- src/Service/TemperatureSensorService.php
+++ src/Service/TemperatureSensorService.php
@@ -6,5 +6,44 @@ namespace App\Service;
 
 class TemperatureSensorService
 {
+    use Singleton;
 
+    /**
+     * @return array
+     */
+    public function getSensors()
+    {
+        $sensors = shell_exec('ls /sys/bus/w1/devices');
+        $sensors = preg_split('/\s+/', $sensors);
+
+        foreach($sensors as $key => $sensor)
+        {
+            if($sensor === "w1_bus_master1" || $sensor === "")
+            {
+                unset($sensors[$key]);
+            }
+        }
+
+        return $sensors;
+    }
+
+    /**
+     * @param array $content
+     * @param array $sensors
+     *
+     * @return array
+     */
+    public function getSensorData(array $content, array $sensors)
+    {
+        foreach($sensors as $key => $sensor)
+        {
+            $result = shell_exec('cat /sys/bus/w1/devices/' . $sensor . '/w1_slave');
+
+            preg_match('/t=\d+/', $result, $matches);
+
+            $content[] = trim($matches[0], 't=');
+        }
+
+        return $content;
+    }
 }
